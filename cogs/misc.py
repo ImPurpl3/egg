98a4539b260dc5ad62a8bf3a437c0e0d34a5e030
vveeps <54472340+vveeps@users.noreply.github.com>
2021-11-22 22:00:52 +0200

    nvm use escape_mentions instead

--- cogs/misc.py
+++ cogs/misc.py
@@ -43,7 +43,7 @@ import parsedatetime as pdt
 from discord import AllowedMentions
 from discord.ext import commands
 from discord.ext.commands import BadArgument, Cog, CommandError, Context
-from discord.utils import escape_markdown, find, sleep_until
+from discord.utils import escape_markdown, escape_mentions, find, sleep_until
 from PIL import Image, ImageDraw, ImageFont
 from youtube_dl import YoutubeDL
 
@@ -861,7 +861,7 @@ class Misc(Cog):
 
     @commands.command(aliases=["pick"])
     async def choose(self, ctx: Context, *options: str):
-        await ctx.send(random.SystemRandom().choice(options), allowed_mentions=AllowedMentions.none())
+        await ctx.send(escape_mentions(random.SystemRandom().choice(options)))
 
 
 def setup(bot: utils.Bot):
